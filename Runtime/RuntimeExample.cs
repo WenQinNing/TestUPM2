0b9c9e4db297130feceecf63ee8372c169aef292
wenqinning <1095471150@qq.com>
2021-01-11 18:00:54 +0800

    添加测试脚本和程序集

---
+++ Runtime/RuntimeExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeExample : MonoBehaviour
+{
+    public void PrintStr(int i)
+    {
+        print(" RuntimeExample " + i);
+    }
+}
